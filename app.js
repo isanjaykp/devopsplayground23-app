a9f9319dada6b104713c76a34f51b039addfafe6
isanjaykp <isanjaykp@gmail.com>
2019-01-19 02:46:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('<html>Hello world</html>'))
+app.get('/', (req, res) => res.send('<html>Hello world SERVERLESS!</html>'))
 module.exports = app
